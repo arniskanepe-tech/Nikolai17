035cc206af27d8d3271a2ba60c858d01c5308c26
arniskanepe-tech <arnis.kanepe@gmail.com>
2026-01-15 16:19:23 +0200

    Change answer from '151' to '149' in game.js

--- assets/game.js
+++ assets/game.js
@@ -27,7 +27,7 @@
       title: "",
       background: "bg1.jpg",
       targetSlot: 0,      // ★
-      answer: "151",
+      answer: "149",
       cardHtml: `
         <p>Steady, Dress up, Go!</p>
         <p class="muted">Uzgriez kodu pretī izvēlētajam simbolam.</p>
